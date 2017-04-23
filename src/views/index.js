0e8333a27b9d93052f3a8af818a83bb5d57751bc
Aford <244734834@qq.com>
2017-04-23 13:11:14 +0800

    rename maintain module

---
+++ src/views/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { HashRouter as Router, Switch } from 'react-router-dom'
+import AnimateRoute from 'COMPONENT/AnimateRoute'
+import Mismatch from './Mismatch'
+import Login from './Login'
+import Main from './Main'
+import 'STYLE'
+
+const App = () => (
+  <Router basename='/'>
+    <Switch>
+      <AnimateRoute transition='slide' exact path='/' component={Mismatch} />
+      <AnimateRoute transition='slide' path='/login' component={Login} />
+      <AnimateRoute transition='slide' path='/main' component={Main} />
+      <AnimateRoute transition='slide' component={Mismatch} />
+    </Switch>
+  </Router>
+)
+
+export default App
