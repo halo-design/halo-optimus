2ad85cea609a3e255a69725dd24afb3f4d459bbb
Aford <244734834@qq.com>
2017-05-03 10:45:59 +0800

    reset hard

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp')
+var sftp = require('gulp-sftp')
+ 
+gulp.task('deploy', function () {
+  return gulp.src('dist/**')
+  .pipe(sftp({
+    host: '139.224.128.69',
+    auth: 'nginxAdmin',
+    remotePath: '/home/nginx/www/ydkf/inmanage'
+  }))
+})
+
+gulp.task('diff', function () {
+  return gulp.src('dist_diff/**')
+  .pipe(sftp({
+    host: '139.224.128.69',
+    user: 'nginx',
+    auth: 'nginxAdmin',
+    remotePath: '/home/nginx/www/ydkf/inmanage'
+  }))
+})
