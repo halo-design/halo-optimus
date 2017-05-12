c073489c24cba0b1f4539673aae5fcfff1ecb91c
Aford <244734834@qq.com>
2017-05-12 11:02:24 +0800

    fix file name

--- src/views/ApplyHistoryList/sync.js
+++ src/views/ApplyHistoryList/sync.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Lazy from 'COMPONENT/Lazy'
 import Loading from 'COMPONENT/Loading'
-import loadApplyHistoryList from 'bundle-loader?lazy&name=spplyHistoryList!./index'
+import loadApplyHistoryList from 'bundle-loader?lazy&name=applyHistoryList!./index'
 
 const ApplyHistoryList = () => (
   <Lazy load={loadApplyHistoryList}>
