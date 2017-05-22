a6253d3965c54b31be41117de2f94561c9c9c3ed
Aford <244734834@qq.com>
2017-05-22 16:10:12 +0800

    move some config to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
+const fs = require('fs')
+const ip = require('ip')
+const opn = require('opn')
+const pug = require('pug')
 const gulp = require('gulp')
 const path = require('path')
 const chalk = require('chalk')
 const sftp = require('gulp-sftp')
+const express = require('express')
 const tools = require('./bin/analyzing-tools')
 
 gulp.task('upload', () => 
@@ -23,7 +28,7 @@ gulp.task('upload-diff', () =>
   }))
 )
 
-gulp.task('snapshot',  () => {
+gulp.task('snapshot', () => {
   tools.getSnapshots('dist', 'data/snapshots')
 })
 
@@ -47,8 +52,33 @@ gulp.task('compare', () => {
       logPath: 'data',
       callback: data => tools.distDiffer(data, 'dist_diff')
     })
-    require('./bin/server-report')
   } else {
     console.log(chalk.red('Can not be compared!'))
   }
 })
+
+gulp.task('server-report', () => {
+  const server = express()
+  const rootPath = path.resolve()
+  const layout = fs.readFileSync('./static/report.html', 'utf8')
+  const viewFn = pug.compileFile('./static/report.pug')
+  const Data = tools.readJSON(path.join(rootPath, 'data/compare.json'))
+  tools.deleteEmptyProperty(Data)
+  const reportView = viewFn(Data)
+  const finalReport = layout.replace('<div id="app"></div>', reportView)
+
+  const uri = `http://${ip.address()}:5000`
+  server.use('/static', express.static(path.join(rootPath, 'static')))
+  server.get('*', (req, res) => {
+    res.send(finalReport)
+  })
+  server.listen(5000, error => {
+    if (error) {
+      throw error
+    }
+    console.log(chalk.green(`Server is running at ${uri}`))
+    opn(uri)
+  })
+})
+
+gulp.task('compare-report', ['compare', 'server-report'])
