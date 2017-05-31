c962c8121de99dd5995987c228d1725c4017dcc6
Aford <244734834@qq.com>
2017-05-31 17:45:13 +0800

    add life cycle template

---
+++ static/lifeCycle.js
@@ -0,0 +1,34 @@
+class LifeCycle extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  componentWillMount () {
+  }
+
+  componentDidMount () {
+  }
+
+  componentWillReceiveProps (nextProps) {
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    return true
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+  }
+
+  componentDidUpdate () {
+  }
+
+  componentWillUnmount () {
+  }
+
+  render () {
+    return null
+  }
+}
