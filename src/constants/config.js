e57453f7b6d7cd5188f12a9b55b579cbe5a8b73c
Aford <244734834@qq.com>
2017-06-02 11:17:56 +0800

    update:fix location path -hzbank

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,12 +4,12 @@ const rootPath = 'halo'
 const serverRootPath = 'inmanage'
 const projectName = 'IFP内部管理系统'
 
-export const routeRootPath = isProd ? `/${rootPath}` : rootPath === serverRootPath ? '/' : rootPath
+export const routeRootPath = isProd ? `/${rootPath}` : rootPath === serverRootPath ? '/' : `/${rootPath}`
 
 export const errorCodeCheck = errorCode => {
   // 数据校验失败返回登录页
   if (errorCode === 'BLEC0001' || errorCode === 'SYEC0002') {
-    window.location.replace('/' + routeRootPath)
+    window.location.replace(routeRootPath)
   }
 }
 
